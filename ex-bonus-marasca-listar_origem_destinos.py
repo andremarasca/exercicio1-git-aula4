4a4ac93c46b2ae16d235142eac94a842807a9a14
André Luiz Marasca <andremarasca@outlook.com>
2021-04-27 16:21:07 -0300

    Apaguei 2 linhas de comentario, desnecessárias

--- ex-bonus-marasca-listar_origem_destinos.py
+++ ex-bonus-marasca-listar_origem_destinos.py
@@ -4,10 +4,6 @@ import pandas as pd
 
 df_sample = pd.read_csv("data/2006-sample.csv", encoding="latin-1", dtype=str)
 
-
-# Origin
-# Dest
-
 # Criar um dicionario (chave: Nome do aeropoto, conteúdo: lista vazia)
 
 dict_origin_dest = {}
@@ -18,9 +14,10 @@ for key in df_sample["Origin"].unique():
 # Iterar o DataFrame populando o dicionário dict_origin_dest
 
 for idx, row in df_sample.iterrows():
-    dict_origin_dest[row["Origin"]].append(row["Dest"]) 
+    dict_origin_dest[row["Origin"]].append(row["Dest"])
 
 # Imprimir o dicionario dict_origin_dest
 
 for key in dict_origin_dest:
-    print("Origem", key, "==> Destino:", ", ".join(dict_origin_dest[key]), "\n")
+    print("Origem", key, "==> Destino:",
+          ", ".join(dict_origin_dest[key]), "\n")
